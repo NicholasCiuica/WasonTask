f02f2a4eeffb452b619b4a8409ead27894e3bcf2
NicholasCiuica <nick.ciuica@gmail.com>
2025-03-30 21:21:17 -0400

    flippable cards

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WasonTask
+
+Sources used: 
+
+For flippable card css:
+https://www.w3schools.com/howto/howto_css_flip_card.asp
--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const cardContainer = document.querySelector
+const cardContainer = document.querySelector("#card-container");
+let cardContainerWidth = cardContainer.clientWidth;
+let cardContainerHeight = cardContainer.clientHeight;
+
+const cardList = [];
+
+{/* <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div> */}
+
+for(let i = 0; i < 4; i++) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  cardList.push(cardContainer.appendChild(card));
+
+  const cardInner = document.createElement("div");
+  cardInner.classList.add("card-inner");
+  card.appendChild(cardInner);
+
+  const cardFront = document.createElement("div");
+  cardFront.classList.add("card-front");
+  cardInner.appendChild(cardFront);
+
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("card-back");
+  cardInner.appendChild(cardBack);
+}
