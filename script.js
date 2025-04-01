2f1e79c74dcc4cce11f259e71c5f577b42ceab48
NicholasCiuica <nick.ciuica@gmail.com>
2025-03-31 20:05:33 -0400

    added flip functionality and buttons

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
 const cardContainer = document.querySelector("#card-container");
-let cardContainerWidth = cardContainer.clientWidth;
-let cardContainerHeight = cardContainer.clientHeight;
+
+//contains card-inners because those contain the card-fronts
+//and card-backs and because transformations are to be done on 
 
 const cardList = [];
 
-{/* <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div> */}
-
-for(let i = 0; i < 4; i++) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  cardList.push(cardContainer.appendChild(card));
-
-  const cardInner = document.createElement("div");
-  cardInner.classList.add("card-inner");
-  card.appendChild(cardInner);
-
-  const cardFront = document.createElement("div");
-  cardFront.classList.add("card-front");
-  cardInner.appendChild(cardFront);
-
-  const cardBack = document.createElement("div");
-  cardBack.classList.add("card-back");
-  cardInner.appendChild(cardBack);
+function setCards() {
+  //delete any old cards
+  cardList.length = 0;
+  cardContainer.innerHTML = "";
+  //create the four flippable card objects
+  for(let i = 0; i < 4; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    cardContainer.appendChild(card);
+  
+    const cardInner = document.createElement("div");
+    cardInner.classList.add("card-inner");
+    card.appendChild(cardInner);
+    cardList.push(cardInner);
+  
+    cardInner.onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  
+    const cardFront = document.createElement("div");
+    cardFront.classList.add("card-front");
+    cardFront.textContent = "A";
+    cardInner.appendChild(cardFront);
+  
+    const cardBack = document.createElement("div");
+    cardBack.classList.add("card-back");
+    cardBack.textContent = "B";
+    cardInner.appendChild(cardBack);
+  }
+  
+  //give the cards their values
+  const cardValues = getNormalWasonValues();
+  for(let i = 0; i < 4; i++) {
+    cardList[i].querySelector(".card-front").textContent = cardValues[i].front;
+    cardList[i].querySelector(".card-back").textContent = cardValues[i].back;
+    if(cardValues[i].relevant === true) {
+      cardList[i].classList.add("relevant");
+    }
+  }
 }
+setCards();
+
+//button that flips selected cards
+document.querySelector("#flip-button").onclick = () => {
+  for(let i = 0; i < 4; i++) {
+    if(cardList[i].classList.contains("clicked")) {
+      cardList[i].classList.remove("clicked");
+      cardList[i].classList.add("flipped");
+    }
+  }
+};
+
+//unflip all flipped buttons
+document.querySelector("#unflip-button").onclick = () => {
+  for(let i = 0; i < 4; i++) {
+    cardList[i].classList.remove("flipped");
+  }
+};
+
+//reset the cards
+document.querySelector("#reset-button").onclick = setCards;
---
+++ util.js
@@ -0,0 +1,64 @@
+const evenNumbers = Array(5).fill(0).map((x, i) => 2*(i + 1));
+const oddNumbers = Array(5).fill(0).map((x, i) => 2*i + 1);
+const numbers = Array(10).fill(0).map((x, i) => i + 1);
+const vowels = Array(5).fill(0).map((x, i) => "AEIOU".charAt(i));
+//not including Y to avoid confusion
+const consonants = Array(20).fill(0).map((x, i) => "BCDFGHJKLMNPQRSTVWXZ".charAt(i));
+const letters = Array(25).fill(0).map((x, i) => "ABCDEFGHIJKLMNOPQRSTUVWXZ".charAt(i));
+
+//examples, E rating, alcoholic beverages
+//cheater detection
+//unfair leeching resources -> evolutionarily advantageous to
+//spot cheaters, people not being cooperative in group, better
+//group survival
+
+https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getNormalWasonValues() {
+  const cardValues = [];
+  shuffle(evenNumbers);
+  shuffle(oddNumbers);
+  shuffle(numbers);
+  shuffle(vowels);
+  shuffle(consonants);
+  shuffle(letters);
+
+  cardValues.push({
+    "front": vowels[0],
+    "back": numbers[0],
+    "relevant": false
+  });
+  cardValues.push({
+    "front": consonants[0],
+    "back": numbers[1],
+    "relevant": true
+  });
+  cardValues.push({
+    "front": evenNumbers[1],
+    "back": letters[0],
+    "relevant": true
+  });
+  cardValues.push({
+    "front": oddNumbers[1],
+    "back": letters[1],
+    "relevant": false
+  });
+
+  shuffle(cardValues);
+  return cardValues;
+}
